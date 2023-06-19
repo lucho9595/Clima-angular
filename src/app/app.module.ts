41e093b8eacf28ac7f9e022e0b4ea79ed2c32105
lucho9595 <lucianocoronel48@gmail.com>
2023-06-19 19:22:28 -0300

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,31 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './component/login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { HomeComponent } from './component/home/home.component';
+import { RegisterComponent } from './component/register/register.component';
+const firebaseConfig = {
+  apiKey: "AIzaSyB7HwSLdcodR2pVmn6Ut-BvfX4bD3_7SWQ",
+  authDomain: "clima-app-1997.firebaseapp.com",
+  projectId: "clima-app-1997",
+  storageBucket: "clima-app-1997.appspot.com",
+  messagingSenderId: "654000265003",
+  appId: "1:654000265003:web:1ac1f22d7fa6c89e72d76e",
+  measurementId: "G-ND18RM8PVT"
+};
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    provideFirebaseApp(() => initializeApp(firebaseConfig))
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from 'firebase/compat/app';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) { }
 
   ngOnInit(): void {
+
   }
 
+  loginWithGitHub() {
+    this.afAuth.signInWithPopup(new firebase.auth.GithubAuthProvider())
+      .then((result) => {
+        // Aquí puedes manejar la respuesta exitosa y redirigir o realizar otras acciones necesarias
+      })
+      .catch((error) => {
+        // Aquí puedes manejar errores de inicio de sesión
+      });
+  }
+
+  loginWithGoogle() {
+    this.afAuth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
+      .then((result) => {
+        // Aquí puedes manejar la respuesta exitosa y redirigir o realizar otras acciones necesarias
+      })
+      .catch((error) => {
+        // Aquí puedes manejar errores de inicio de sesión
+      });
+  }
 }
